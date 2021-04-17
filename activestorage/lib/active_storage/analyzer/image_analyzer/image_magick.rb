b488e7d4e269da39632b8a7202ca61e829a90147
Breno Gazzola <breno.gazzola@gmail.com>
2021-04-17 16:15:12 -0300

    Remove unecessary comments

--- activestorage/lib/active_storage/analyzer/image_analyzer/image_magick.rb
+++ activestorage/lib/active_storage/analyzer/image_analyzer/image_magick.rb
@@ -1,17 +1,6 @@
 # frozen_string_literal: true
 
 module ActiveStorage
-  # Extracts width and height in pixels from an image blob.
-  #
-  # If the image contains EXIF data indicating its angle is 90 or 270 degrees, its width and height are swapped for convenience.
-  #
-  # Example:
-  #
-  #   ActiveStorage::Analyzer::ImageAnalyzer.new(blob).metadata
-  #   # => { width: 4104, height: 2736 }
-  #
-  # This analyzer relies on the third-party {MiniMagick}[https://github.com/minimagick/minimagick] gem. MiniMagick requires
-  # the {ImageMagick}[http://www.imagemagick.org] system library.
   class Analyzer::ImageAnalyzer::ImageMagick < Analyzer
     def metadata
       read_image do |image|
--- activestorage/lib/active_storage/analyzer/image_analyzer/vips.rb
+++ activestorage/lib/active_storage/analyzer/image_analyzer/vips.rb
@@ -1,19 +1,7 @@
 # frozen_string_literal: true
 
 module ActiveStorage
-  # Extracts width and height in pixels from an image blob.
-  #
-  # If the image contains EXIF data indicating its angle is 90 or 270 degrees, its width and height are swapped for convenience.
-  #
-  # Example:
-  #
-  #   ActiveStorage::Analyzer::ImageAnalyzer.new(blob).metadata
-  #   # => { width: 4104, height: 2736 }
-  #
-  # This analyzer relies on the third-party {MiniMagick}[https://github.com/minimagick/minimagick] gem. MiniMagick requires
-  # the {ImageMagick}[http://www.imagemagick.org] system library.
   class Analyzer::ImageAnalyzer::Vips < Analyzer
-
     def metadata
       read_image do |image|
         if rotated_image?(image)
